951edce33bb1fb06128f11cf37b7d89bcbb1f4e7
terrance456 <terranceprasath97@gmail.com>
2023-12-22 22:17:47 +0800

    bugfix: fix class component issue in swagger ui

--- app/api-doc/react-swagger.tsx
+++ app/api-doc/react-swagger.tsx
@@ -1,14 +1,19 @@
 "use client";
 
-import SwaggerUI from "swagger-ui-react";
+import dynamic from "next/dynamic";
 import "swagger-ui-react/swagger-ui.css";
+
+const DynamicSwaggerUI = dynamic(() => import("swagger-ui-react"), {
+  ssr: false,
+  loading: () => <p>Loading Component...</p>,
+});
 
 type Props = {
   spec: Record<string, any>;
 };
 
 function ReactSwagger({ spec }: Props) {
-  return <SwaggerUI spec={spec} />;
+  return <DynamicSwaggerUI spec={spec} />;
 }
 
 export default ReactSwagger;
