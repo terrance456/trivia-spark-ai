d771f8924e60c925edf9880a0ab77bc58adb95ff
terrance456 <terranceprasath97@gmail.com>
2024-01-05 22:36:32 +0800

    refactor: add theme providers to app

--- src/contexts/Providers.tsx
+++ src/contexts/Providers.tsx
@@ -1,7 +1,16 @@
+import dynamic from "next/dynamic";
 import React, { PropsWithChildren } from "react";
 
+const ThemeProvider = dynamic(() => import("@/src/contexts/ThemeProvider"), { ssr: false, loading: () => <p>Loading Component...</p> });
+
 const Providers: React.FC<any> = ({ children }: PropsWithChildren<any>) => {
-  return <>{children}</>;
+  return (
+    <>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        {children}
+      </ThemeProvider>
+    </>
+  );
 };
 
 export default Providers;
