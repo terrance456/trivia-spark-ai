996277139cb8d82ef128e52c12ac651666a79824
terrance456 <terranceprasath97@gmail.com>
2024-01-09 21:43:34 +0800

    feat: UserProfileDropdown component

--- src/components/Signout/signout.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React from "react";
-import { signOut } from "next-auth/react";
-
-const Signout = () => {
-  return <button onClick={() => signOut()}>Logout</button>;
-};
-
-export default Signout;
--- src/contexts/Providers.tsx
+++ src/contexts/Providers.tsx
@@ -1,15 +1,16 @@
+import { SessionProvider } from "next-auth/react";
 import dynamic from "next/dynamic";
 import React, { PropsWithChildren } from "react";
 
-const ThemeProvider = dynamic(() => import("@/src/contexts/ThemeProvider"), { ssr: false, loading: () => <p>Loading Component...</p> });
+const ThemeProvider = dynamic(() => import("@/src/contexts/ThemeProvider"), { ssr: false, loading: () => <div className="p-5">Loading...</div> });
 
 const Providers: React.FC<any> = ({ children }: PropsWithChildren<any>) => {
   return (
-    <>
+    <SessionProvider>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
         {children}
       </ThemeProvider>
-    </>
+    </SessionProvider>
   );
 };
 
