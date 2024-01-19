e92212a7a2ce262c38d23c313d74b5d8d43bb699
terrance456 <terranceprasath97@gmail.com>
2024-01-19 21:00:48 +0800

    refactor: add global setting context

--- src/contexts/Providers.tsx
+++ src/contexts/Providers.tsx
@@ -1,6 +1,7 @@
 import { SessionProvider } from "next-auth/react";
 import dynamic from "next/dynamic";
 import React, { PropsWithChildren } from "react";
+import { GlobalSettingProvider } from "./GlobalSettingContext";
 
 const ThemeProvider = dynamic(() => import("@/src/contexts/ThemeProvider"), { ssr: false, loading: () => <div className="p-5">Loading...</div> });
 
@@ -8,7 +9,7 @@ const Providers: React.FC<any> = ({ children }: PropsWithChildren<any>) => {
   return (
     <SessionProvider>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-        {children}
+        <GlobalSettingProvider>{children}</GlobalSettingProvider>
       </ThemeProvider>
     </SessionProvider>
   );
