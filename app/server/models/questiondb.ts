aa5c04632f69db87654066d529b6cd03560f62f3
terrance456 <terranceprasath97@gmail.com>
2024-03-23 22:22:14 +0800

    refactor: remove existing mongo models

--- app/server/models/questiondb.ts
+++
@@ -1,9 +0,0 @@
-import { ObjectId } from "mongodb";
-
-export interface QuestionDB {
-  _id: ObjectId;
-  question_title: string;
-  topic_id: ObjectId;
-  correct_asnwer_id: string;
-  created_at: number;
-}
--- app/server/models/topicdb.ts
+++
@@ -1,9 +0,0 @@
-import { ObjectId } from "mongodb";
-
-export interface TopicDB {
-  _id: ObjectId;
-  topic_name: string;
-  created_at: Date;
-  no_of_question: number;
-  counts: number;
-}
